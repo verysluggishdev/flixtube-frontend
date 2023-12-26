54404bd3d5578a039a01f345819a16710dcc509b
verysluggishdev <verysluggishdev@gmail.com>
2023-12-26 08:00:33 +0300

    Added chanelID to Post component and modified some classes

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -4,13 +4,13 @@ import './post.css'
 const Post = ({postData}) => {
   console.log(postData)
   return (
-    <div className="video-tile">
+    <div className="post">
       <img src={`http://localhost:8000/media/${postData.post.thumbnail}`} alt="" className='video-thumbnail'/>
-      <div className="video-details">
+      <div className="post-details">
         <img src={`http://localhost:8000/media/${postData.avatar}`} alt="" className="creator-avatar"/>
-        <div className="video-info">
-          <p className="video-title">{postData.post.title}</p>
-          <p className="video-creator">{postData.post.channelID}</p>
+        <div className="post-info">
+          <p className="post-title">{postData.post.title}</p>
+          <p className="channelID">@{postData.channelID}</p>
           <div className="flex-container">
             <p className="view-count">100k Views</p>
             <p className="upload-date">Uploaded {postData.post.created_at}</p>
